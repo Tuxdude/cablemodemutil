4cab6bf3f147a25e6c3cfdce9c5c182147621334
Ash <tuxdude@Baymax.area51>
2022-01-25 23:28:30 -0800

    Implement the initial part of login.
    1. The login internal function prepares the initial request to obtain the
    uid, public key, challenge from the cable modem.
    2. The challenge, public key and the user supplied password are then
       used to compute the private key.
    3. The uid and the computed private key are then returned in the token
       along with the expiry timestamp (set to 10 minutes from when the
       credentials were generated).

--- hnap1.go
+++ hnap1.go
@@ -12,3 +12,23 @@ const (
 func actionURI(action string) string {
 	return fmt.Sprintf("\"%s/%s\"", soapNamespace, action)
 }
+
+// Generates the key which contains the response for the SOAP action in the payload.
+func actionResponseKey(action string) string {
+	return fmt.Sprintf("%sResponse", action)
+}
+
+// Generates the key which contains the result of the SOAP action in the payload.
+func actionResultKey(action string) string {
+	return fmt.Sprintf("%sResult", action)
+}
+
+// Generates the private key using the public key, challenge and the clear password.
+func genPrivateKey(publicKey string, challenge string, clearPassword string) string {
+	return genHMACMD5(publicKey+clearPassword, challenge)
+}
+
+// Generates the hashed password using the private key, challenge and the clear password.
+func genHashedPassword(privateKey string, challenge string, clearPassword string) string {
+	return genHMACMD5(privateKey, challenge)
+}
--- retriever.go
+++ retriever.go
@@ -1,12 +1,16 @@
 package cablemodemutil
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"time"
 )
 
 const (
-	urlFormat = "%s://%s/HNAP1/"
+	urlFormat           = "%s://%s/HNAP1/"
+	tokenExpiryDuration = 10 * time.Minute
+	loginAction         = "Login"
 )
 
 // Retriever is used to retrieve the current status of the Cable Modem.
@@ -40,11 +44,81 @@ type token struct {
 	expiry time.Time
 }
 
+// Initial response from the cable modem to allow the client to initiate authentication.
+type loginResponse struct {
+	// The UID of the session provided by the cable modem during authentication.
+	uid string
+	// The public key provided by the cable modem during authentication.
+	publicKey string
+	// The challenge message provided by the cable modem during authentication.
+	challenge string
+}
+
+// actionRequest represents the payload of the request containing the SOAP action command.
+type actionRequest map[string]string
+
+// actionResponse represents the payload of the response to a SOAP action command request.
+type actionResponse map[string]interface{}
+
+// soapRequest represents the full SOAP request body.
+type soapRequest map[string]actionRequest
+
+// soapResponse represents the full SOAP response body to a SOAP request sent.
+type soapResponse map[string]actionResponse
+
 // Returns a token that has been reset to the initial state.
 func resetToken() *token {
 	return &token{
 		privateKey: "withoutLoginKey",
 	}
+}
+
+// Encodes the specified payload for the specified action into a byte buffer to send this as a request.
+func encodePayload(action string, payload actionRequest) *bytes.Buffer {
+	req := soapRequest{
+		action: payload,
+	}
+	buf := new(bytes.Buffer)
+	json.NewEncoder(buf).Encode(req)
+	return buf
+}
+
+// Decodes the specified byte array response for the specified action into the response payload.
+func decodePayload(action string, resp *[]byte) (actionResponse, error) {
+	var payload soapResponse
+	err := json.Unmarshal(*resp, &payload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode response payload, reason:%w", err)
+	}
+	return unpackResponse(action, payload)
+}
+
+// Validates and unpacks the response for the specified SOAP action.
+func unpackResponse(action string, resp soapResponse) (actionResponse, error) {
+	if len(resp) != 1 {
+		return nil, fmt.Errorf(
+			"action: %s, invalid number of keys (%d) in response, expected 1.\nresponse: %v", action, len(resp), prettyPrintJSON(resp))
+	}
+
+	respKey := actionResponseKey(action)
+	unpacked, ok := resp[respKey]
+	if !ok {
+		return nil, fmt.Errorf(
+			"action: %s, unable to find the response key %q in response.\nresponse: %v", action, respKey, prettyPrintJSON(resp))
+	}
+
+	resultKey := actionResultKey(action)
+	result, ok := unpacked[resultKey]
+	if !ok {
+		return nil, fmt.Errorf(
+			"action: %s, unable to find the result key %q in unpacked response.\nunpacked response: %v", action, resultKey, prettyPrintJSON(unpacked))
+	}
+	if result != "OK" {
+		return nil, fmt.Errorf(
+			"action: %s, result in unpacked resposne is %q, expected \"OK\".\nunpacked response: %v", action, result, prettyPrintJSON(unpacked))
+	}
+
+	return unpacked, nil
 }
 
 // Builds a new Retriever object to query the Cable Modem.
@@ -55,6 +129,72 @@ func NewStatusRetriever(input *RetrieverInput) *Retriever {
 	r.username = input.Username
 	r.clearPassword = input.ClearPassword
 	return &r
+}
+
+// Sends the SOAP request for the specified action containing the specified payload.
+func (r *Retriever) sendReq(action string, payload actionRequest, tok *token) (actionResponse, error) {
+	req := encodePayload(action, payload)
+	resp, err := r.client.sendPOST(action, req, tok)
+	if err != nil {
+		return nil, err
+	}
+	return decodePayload(action, resp)
+}
+
+// Retrieves the cookie, public key and challenge information from the cable modem that can be used for initiating an authentication request.
+func (r *Retriever) getLoginResponse() (*loginResponse, error) {
+	payload := actionRequest{
+		"LoginPassword": "",
+		"Captcha":       "",
+		"PrivateLogin":  "LoginPassword",
+		"Action":        "request",
+		"Username":      r.username,
+	}
+	tok := resetToken()
+	resp, err := r.sendReq(loginAction, payload, tok)
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve login challenge\nreason: %w", err)
+	}
+
+	var ok bool
+	result := loginResponse{}
+	result.uid, ok = resp["Cookie"].(string)
+	if !ok {
+		return nil, fmt.Errorf(
+			"unable to find key 'Cookie' in login response\nresponse: %v", prettyPrintJSON(resp))
+	}
+
+	result.publicKey, ok = resp["PublicKey"].(string)
+	if !ok {
+		return nil, fmt.Errorf(
+			"unable to find key 'PublicKey' in login response\nresponse: %v", prettyPrintJSON(resp))
+	}
+
+	result.challenge, ok = resp["Challenge"].(string)
+	if !ok {
+		return nil, fmt.Errorf(
+			"unable to find key 'Challenge' in login response\nresponse: %v", prettyPrintJSON(resp))
+	}
+
+	return &result, nil
+}
+
+// Login to the cable modem using the specified username and password.
+func (r *Retriever) login() (*token, error) {
+	loginResp, err := r.getLoginResponse()
+	if err != nil {
+		return nil, err
+	}
+	// Compute the expiry time as soon as we obtain the response.
+	expiry := time.Now().Add(tokenExpiryDuration)
+
+	privateKey := genPrivateKey(loginResp.publicKey, loginResp.challenge, r.clearPassword)
+	tok := &token{
+		uid:        loginResp.uid,
+		privateKey: privateKey,
+		expiry:     expiry,
+	}
+	return tok, nil
 }
 
 // Retrieves the current detailed raw status from the cable modem.
