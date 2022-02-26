fe8292b8d294a6abb6c744b9e9fb4854ea72dbbd
Ash <tuxdude@Baymax.area51>
2022-02-26 01:53:01 -0800

    build: Move to the self-upgradable makesystem.

--- .bootstrap/setup-makesystem.sh
+++ .bootstrap/setup-makesystem.sh
@@ -4,20 +4,34 @@ set -e -o pipefail
 
 ver="${1:?}"
 makesystem_dir="${2:?}"
+upgrade=0
+if [[ "${3}" == "--upgrade" ]]; then
+    upgrade=1
+fi
+
 makesystem_id="${makesystem_dir:?}/.id"
 
-if [[ -f "${makesystem_id:?}" ]] && [[ "makesystem" == "$(cat ${makesystem_id:?})"  ]]; then
+if [[ "${upgrade:?}" != "1" ]] && [[ -f "${makesystem_id:?}" ]] && [[ "makesystem" == "$(cat ${makesystem_id:?})"  ]]; then
     echo "Makesystem installation already detected, no changes made"
     exit 0
 fi
 
 if [[ ${makesystem_dir:?} != "./.makesystem" ]]; then
-    echo "Cannot install the makesystem to a directory other than \"./.makesystem\", \"${makesystem_dir:?}\" was specified instead"
+    echo "Cannot install/upgrade the makesystem to a directory other than \"./.makesystem\", \"${makesystem_dir:?}\" was specified instead"
     exit 1
 fi
 
-echo "Setting up makesystem@v${ver:?} ==> \"${1:?}\""
+if [[ "${upgrade:?}" == "1" ]]; then
+    echo "Setting up (upgrade) makesystem@v${ver:?} ==> \"${makesystem_dir:?}\""
+else
+    echo "Setting up (install) makesystem@v${ver:?} ==> \"${makesystem_dir:?}\""
+fi
+
 rm -rf "${makesystem_dir:?}"
 git clone --quiet --depth 1 --branch v${ver:?} https://github.com/Tuxdude/makesystem.git ${makesystem_dir:?} >/dev/null 2>&1
 rm -rf ${makesystem_dir:?}/.git
+
+${makesystem_dir:?}/scripts/post-install.sh
+
+echo "${ver:?}" > .bootstrap/VERSION
 exit 0
